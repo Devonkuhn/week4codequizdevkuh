e85ebc27ed3dcfd236cd2ab4c962fb0d224553e2
kuhndev <86081460+immortalized513@users.noreply.github.com>
2021-08-21 22:53:11 -0400

    added javascript
    added questions to the questions skeleton

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,41 +1,42 @@
 var quizQuestions = [{
 
-    question:"",
-    optionA:"",
-    optionB:"",
-    optionC:"",
-    optionD:"",
-    correctAwnser:""},
+    question:"Commonly used data types DO NOT include:",
+    optionA:"numbers",
+    optionB:"alerts",
+    optionC:"strings",
+    optionD:"classes",
+    correctAwnser:"b"},
     {
 
-    question:"",
-    optionA:"",
-    optionB:"",
-    optionC:"",
-    optionD:"",
-    correctAwnser:""},
+    question:"The condition in an if / else statement is enclosed within _______.",
+    optionA:"parentheses",
+    optionB:"square brackets",
+    optionC:"quotes",
+    optionD:"curly brackets",
+    correctAwnser:"a"},
     {
 
-    question:"",
-    optionA:"",
-    optionB:"",
-    optionC:"",
-    optionD:"",
-    correctAwnser:""},
+    question:"Arrays in Javascript can be used to store ______.",
+    optionA:"booleans",
+    optionB:"numbers and strings",
+    optionC:"other arrays",
+    optionD:"all of the above",
+    correctAwnser:"d"},
     {
 
-    question:"",
-    optionA:"",
-    optionB:"",
-    optionC:"",
-    optionD:"",
-    correctAwnser: },
+    question:"String values must be enclosed within ______ when being assigned to variables.",
+    optionA:"parentheses",
+    optionB:"commas",
+    optionC:"quotes",
+    optionD:"curly brackets",
+    correctAwnser:"c"},
     {      
     
-    question:"",
-    optionA:"",
-    optionB:"",
-    optionC:"",
-    optionD:"",
-    correctAwnser:""},
-    {   
+    question:"The _______ is a very useful tool. It's used during devlopment, debugging and for printing content to the debugger.",
+    optionA:"console log",
+    optionB:"terminal / bash",
+    optionC:"CSS",
+    optionD:"C++",
+    correctAwnser:"a"},
+
+    ];
